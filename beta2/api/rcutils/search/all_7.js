e27dd08c0ee56beaa76068a18e35c6dd6ae62fe7
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-07-05 14:07:58 -0400

    Run doxygen after rcutils build.

--- beta2/api/rcutils/search/all_7.js
+++ beta2/api/rcutils/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['line_5fnumber',['line_number',['../structrcutils__error__state__t.html#ac84687d24363e3b1a1bd7279ed86458e',1,'rcutils_error_state_t::line_number()'],['../structrcl__log__location__t.html#adcf53fdc2c99689da1f453a6242cb3b8',1,'rcl_log_location_t::line_number()']]],
-  ['logging_2eh',['logging.h',['../logging_8h.html',1,'']]]
+  ['logging_2eh',['logging.h',['../logging_8h.html',1,'']]],
+  ['logging_5fmacros_2eh',['logging_macros.h',['../logging__macros_8h.html',1,'']]]
 ];
--- beta2/api/rcutils/search/typedefs_0.js
+++ beta2/api/rcutils/search/typedefs_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['rcl_5flog_5flocation_5ft',['rcl_log_location_t',['../logging_8h.html#a4227f76ce7f81fc1fe56353d690dca67',1,'logging.h']]],
-  ['rcl_5flogging_5foutput_5fhandler_5ft',['rcl_logging_output_handler_t',['../logging_8h.html#a25c5339254914f686686ea5f826af895',1,'logging.h']]],
+  ['rcl_5flogging_5foutput_5fhandler_5ft',['rcl_logging_output_handler_t',['../logging_8h.html#a6d03491822b643dd8f37980c9503fcc3',1,'logging.h']]],
+  ['rcllogfilter',['RclLogFilter',['../logging__macros_8h.html#a9ee576d7a9ea193c8806a6a6039683f7',1,'logging_macros.h']]],
   ['rcutils_5fallocator_5ft',['rcutils_allocator_t',['../allocator_8h.html#a3e3829ddfe4fa3e0690affaff7954eec',1,'allocator.h']]],
   ['rcutils_5fduration_5fvalue_5ft',['rcutils_duration_value_t',['../time_8h.html#a7fd03e84bd0de8c6e189bf2a21cf3946',1,'time.h']]],
   ['rcutils_5ferror_5fstate_5ft',['rcutils_error_state_t',['../error__handling_8h.html#a54602d3d4b7a5b19ba2be0baeef1c59e',1,'error_handling.h']]],
